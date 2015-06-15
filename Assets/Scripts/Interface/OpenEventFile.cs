0a2195b36d2589ea5caa8f7cd591d3b2d409889e
Alistair McLean <amclean@nmsu.edu>
2015-06-15 15:26:38 -0500

    Test

---
+++ Assets/Scripts/Interface/OpenEventFile.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenEventFile : MonoBehaviour {
+
+	void Start () 
+	{
+	
+	}
+
+	public string fileName;
+
+	void Update () 
+	{		
+		string realPath = Application.persistentDataPath + "/Phone/VENu/" + fileName;
+		
+		if (!System.IO.File.Exists(realPath))
+		{
+			if (!System.IO.Directory.Exists(Application.persistentDataPath + "/Phone/VENu/"))
+			{
+				System.IO.Directory.CreateDirectory(Application.persistentDataPath + "/Phone/VENu/");
+			}
+			
+			WWW reader = new WWW(Application.streamingAssetsPath + "/Phone/VENu/" + realPath);
+			while ( ! reader.isDone) {}
+			
+			System.IO.File.WriteAllBytes(realPath, reader.bytes);
+		}
+		
+		Application.OpenURL(realPath);
+
+	}
+
+}
