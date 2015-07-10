e5353de4ac6a80c2662a480e4c1337e512bbb408
OwenCrawford <owen@owens-air.dhcp.fnal.gov>
2015-07-10 11:45:05 -0500

    more testing
    Ingame menu and potential camera controls

--- Assets/Scripts/Interface/Event Menu/EventButtonScript.js
+++ Assets/Scripts/Interface/Event Menu/EventButtonScript.js
@@ -1,10 +1,12 @@
 ﻿#pragma strict
 
 var fileName : String;
+public var levelToLoad : String;
 
 public function OnClick(){
 
 	//choose the .json file to be loaded
 	PlayerPrefs.SetString("File To Load", fileName);
-	//Debug.Log("chose file " + fileName);
+	Debug.Log("Loading event " + PlayerPrefs.GetString("File To Load"));
+	Application.LoadLevel(levelToLoad); //replace with appropriate level?
 }
