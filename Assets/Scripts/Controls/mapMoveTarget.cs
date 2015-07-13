ce24fc85534216d5754e9eadc2a82e561d2c7548
Alistair McLean <amclean@nmsu.edu>
2015-07-13 10:33:09 -0500

    More cleaning

--- Assets/Scripts/Controls/mapMoveTarget.cs
+++ Assets/Scripts/Controls/mapMoveTarget.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 
 public class mapMoveTarget : MonoBehaviour {
-	public 
+	public TouchPad miniMap;
+	public GameObject target;
+
 
 
 	// Use this for initialization
@@ -12,11 +14,17 @@ public class mapMoveTarget : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//transform.Translate.x 
+
+
 		if (Input.touchCount > 0) {
-			// The screen has been touched so store the touch
+			// The screen has been touched so 	 the touch
 			Touch touch = Input.GetTouch(0);
 			
-			if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved) {
+			if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved ) {
+				Ray ray = Camera.main.ScreenPointToRay(touch.position);
+				RaycastHit2D hit;
+				//if(Physics.Raycast(ray, out hit
 				// If the finger is on the screen, move the object smoothly to the touch position
 				Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));                
 				transform.position = Vector3.Lerp(transform.position, touchPosition, Time.deltaTime);
