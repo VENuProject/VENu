af1bc2f798fda8b12cad085668f96b682ce02121
OwenCrawford <owen@owens-air.dhcp.fnal.gov>
2015-07-21 10:33:53 -0500

    Hard-coded android files

--- Assets/Scripts/Interface/Event Menu/FileGatherer.js
+++ Assets/Scripts/Interface/Event Menu/FileGatherer.js
@@ -13,40 +13,37 @@ public var displayLevel : String;
 
 function Start () {
 
-	//I don't know how Android's WWW works, and I can't find any documentation on it
-	//Maybe this will work, but I'll have to have one of you explain it to me.
-	// -Owen
 	if (Application.platform == RuntimePlatform.Android){
-		var jsonFilesPath = "jar:file://" + Application.dataPath + "!/assets";
-		    var www : WWW = new WWW(jsonFilesPath); //AMCLEAN add
-   			yield www; // AMCLEAN add
-   			//jsonString=www.text;
-		/*
-		
-		    var url="jar:file://" + Application.dataPath + "!/assets/"+ fileName;
-    Debug.Log(Application.platform+"\n"+url);
-    var www : WWW = new WWW(url);
-    yield www;
-    jsonString=www.text;
-		
-		*/
+	
+//		var jsonFilesPath = "jar:file://" + Application.dataPath + "!/assets";
+//		    var www : WWW = new WWW(jsonFilesPath); //AMCLEAN add
+//   			yield www; // AMCLEAN add
+
+		//until I can get this working on android, hardcoded for now
+
+		AddButton("prod_bnblike_proton_uboone.json");
+		AddButton("prod_eminus_0.1-2.0GeV_isotropic.json");
+		AddButton("prod_eminus_0.5-5.0GeV_25degf_uboone.json");
+		AddButton("prodgenie_bnb_intrinsic_nue_uboone.json");
 	}
-	else jsonFilesPath = Application.streamingAssetsPath; //this works fine on iOS
-    
- 		
-	var dir = new DirectoryInfo(jsonFilesPath);
-	var filesInfo = dir.GetFiles("*.json");
-	for (file in filesInfo)
-		AddButton(file);
-	Debug.Log("found " + filesInfo.Length + " json files");
+	else {
+	
+		jsonFilesPath = Application.streamingAssetsPath; //this works fine on iOS
+    	
+		var dir = new DirectoryInfo(jsonFilesPath);
+		var filesInfo = dir.GetFiles("*.json");
+		for (file in filesInfo)
+			AddButton(file.Name);
+		Debug.Log("found " + filesInfo.Length + " json files");
+	}
 }
 
-function AddButton (file : FileInfo){
+function AddButton (file : String){
 	var newButton : GameObject;
 	newButton = Instantiate(EventButton);
 	newButton.transform.SetParent(ButtonsGroup.transform, false);
-	newButton.GetComponentInChildren(UnityEngine.UI.Text).text = file.Name;
-	newButton.GetComponent(EventButtonScript).fileName = file.Name;
+	newButton.GetComponentInChildren(UnityEngine.UI.Text).text = file;
+	newButton.GetComponent(EventButtonScript).fileName = file;
 	newButton.GetComponent(EventButtonScript).levelToLoad = displayLevel;
 	//custom graphics for each file?
 	//other button customization?
