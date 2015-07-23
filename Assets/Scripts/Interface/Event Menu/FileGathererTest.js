3257e2eae2e7485c3c52e2e85664393751877ebb
OwenCrawford <owen@owens-air.dhcp.fnal.gov>
2015-07-23 16:06:49 -0500

    Working on overhaul of main menu

---
+++ Assets/Scripts/Interface/Event Menu/FileGathererTest.js
@@ -0,0 +1,61 @@
+﻿#pragma strict
+
+import System.IO;
+import UnityEngine.WWW;
+import UnityEngine.UI;
+
+public var jsonFilesPath;
+public var EventButton : GameObject;
+public var ButtonsGroup : GameObject;
+public var displayLevel : String;
+
+function Start () {
+
+	if (Application.platform == RuntimePlatform.Android){
+	
+//		var jsonFilesPath = "jar:file://" + Application.dataPath + "!/assets";
+//		    var www : WWW = new WWW(jsonFilesPath); //AMCLEAN add
+//   			yield www; // AMCLEAN add
+
+		//until I can get this working on android, hardcoded for now
+
+		AddButton("prod_bnblike_proton_uboone.json");
+		AddButton("prod_eminus_0.1-2.0GeV_isotropic.json");
+		AddButton("prod_eminus_0.5-5.0GeV_25degf_uboone.json");
+		AddButton("prodgenie_bnb_intrinsic_nue_uboone.json");
+	}
+	else {
+	
+		jsonFilesPath = Application.streamingAssetsPath; //this works fine on iOS
+    	
+		var dir = new DirectoryInfo(jsonFilesPath);
+		var filesInfo = dir.GetFiles("*.json");
+		for (file in filesInfo)
+			AddButton(file.Name);
+		Debug.Log("found " + filesInfo.Length + " json files");
+	}
+	
+#if MOBILE_INPUT
+	
+	ButtonsGroup.GetComponent(GridLayoutGroup).cellSize = Vector2(120f, 120f);
+	ButtonsGroup.GetComponent(GridLayoutGroup).spacing = Vector2(8, 8);
+	
+#else
+	
+	ButtonsGroup.GetComponent(GridLayoutGroup).cellSize = Vector2(75f, 75f);
+	ButtonsGroup.GetComponent(GridLayoutGroup).spacing = Vector2(8, 8);
+	
+#endif
+	
+}
+
+function AddButton (file : String){
+	var newButton : GameObject;
+	newButton = Instantiate(EventButton);
+	newButton.transform.SetParent(ButtonsGroup.transform, false);
+	newButton.GetComponentInChildren(UnityEngine.UI.Text).text = file;
+	newButton.GetComponent(EventButtonScript).fileName = file;
+	newButton.GetComponent(EventButtonScript).levelToLoad = displayLevel;
+	//custom graphics for each file?
+	//other button customization?
+}
---
+++ Assets/Scripts/Interface/Event Menu/MainMenuScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public GameObject savedEventPanel;
+	public GameObject webEventPanel;
+	public GameObject settingsPanel;
+	public GameObject aboutPanel;
+	public GameObject exitPanel;
+
+	void Start(){
+		HidePanels();
+		ShowSavedEvents();
+	}
+
+	public void HidePanels(){
+		savedEventPanel.SetActive(false);
+		webEventPanel.SetActive(false);
+		settingsPanel.SetActive(false);
+		aboutPanel.SetActive(false);
+		exitPanel.SetActive(false);
+	}
+
+	public void ShowSavedEvents(){
+		HidePanels();
+		savedEventPanel.SetActive(true);
+	}
+
+	public void ShowWebEvents(){
+		HidePanels();
+		webEventPanel.SetActive(true);
+	}
+
+	public void ShowSettings(){
+		HidePanels();
+		settingsPanel.SetActive(true);
+	}
+
+	public void ShowAbout(){
+		HidePanels();
+		aboutPanel.SetActive(true);
+	}
+
+	public void ShowExit(){
+		HidePanels();
+		exitPanel.SetActive(true);
+	}
+}
