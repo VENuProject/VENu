0f96b15b449a5bde4e7d7fc6d07544245f73a232
OwenCrawford <owen@owens-air.dhcp.fnal.gov>
2015-07-27 15:15:14 -0500

    updated InGameMenuScript

--- Assets/Scripts/Interface/InGameMenu/InGameMenuScript.cs
+++ Assets/Scripts/Interface/InGameMenu/InGameMenuScript.cs
@@ -4,7 +4,7 @@ using System.IO;
 using UnityEngine.UI;
 
 
-public class InGameMenuScript: MonoBehaviour {
+public class InGameMenuScriptTest: MonoBehaviour {
 	
 	public string EventMenuScene;
 	
@@ -17,53 +17,58 @@ public class InGameMenuScript: MonoBehaviour {
 	private RectTransform me;
 	const float slideSpeed = 3;
 	public RectTransform buttonsGroup;
-
-
+	public GameObject slideButton;
+	
+	
 	void Start () {
-
+		
 		me = GetComponent<RectTransform>();
-
-#if MOBILE_INPUT
-		me.sizeDelta = new Vector2(80, me.sizeDelta.y);
+		
+		#if MOBILE_INPUT
+		me.sizeDelta = new Vector2(360, me.sizeDelta.y);
 		foreach(LayoutElement child in buttonsGroup.GetComponentsInChildren<LayoutElement>()){
-			child.minHeight = 12;
+			child.minHeight = 75;
 		}
-#else
-		me.sizeDelta = new Vector2(120, me.rect.height);
+		#else
+		me.sizeDelta = new Vector2(320, me.sizeDelta.y);
 		foreach(LayoutElement child in transform.GetComponentsInChildren<LayoutElement>()){
-			child.minHeight = 20;
+			child.minHeight = 70;
 		}
-#endif
-
+		#endif
+		
 		state = menuState.isIn;
 		inPos = -(me.rect.width / 2);
 		outPos = (me.rect.width / 2);
 		me.anchoredPosition = new Vector2(inPos, 0);
-
+		
 	}
-
+	
 	void Update () {
-
+		
 		if(state == menuState.slidingOut){
 			me.anchoredPosition = Vector2.Lerp (new Vector2(inPos, 0), new Vector2(outPos, 0), (Time.time - startTime) * slideSpeed);
+			//slideButton.GetComponent<RectTransform>().eulerAngles = Vector3.Lerp(new Vector3(0, 0, 0), new Vector3(0, 180, 0), (Time.time - startTime) * slideSpeed);
 			if (me.anchoredPosition == new Vector2(outPos, 0))
 				state = menuState.isOut;
 		}
 		else if(state == menuState.slidingIn){
 			me.anchoredPosition = Vector2.Lerp (new Vector2(outPos, 0), new Vector2(inPos, 0), (Time.time - startTime) * slideSpeed);
+			//slideButton.GetComponent<RectTransform>().eulerAngles = Vector3.Lerp(new Vector3(0, 180, 0), new Vector3(0, 0, 0), (Time.time - startTime) * slideSpeed);
 			if (me.anchoredPosition == new Vector2(inPos, 0))
 				state = menuState.isIn;
 		}
-
+		
 	}
-
-	void SlideMenu() {
+	
+	public void SlideMenu() {
 		if(state == menuState.isIn){
 			state = menuState.slidingOut;
+			slideButton.GetComponent<RectTransform>().eulerAngles = new Vector3(0, 0, 180);
 			startTime = Time.time;
 		}
 		else if(state == menuState.isOut){
 			state = menuState.slidingIn;
+			slideButton.GetComponent<RectTransform>().eulerAngles = new Vector3(0, 0, 0);
 			startTime = Time.time;
 		}
 	}
@@ -71,13 +76,13 @@ public class InGameMenuScript: MonoBehaviour {
 	public void ToEventMenu() {
 		Application.LoadLevel (EventMenuScene);
 	}
-
+	
 	public void LoadNext(){
 		string currentEvent = PlayerPrefs.GetString("File To Load");
 		DirectoryInfo dir = new DirectoryInfo(Application.streamingAssetsPath);
 		FileInfo[] filesInfo = dir.GetFiles("*.json");
 		int currentIndex = -1;
-
+		
 		for(int i = 0; i < filesInfo.Length; i++)
 			if(filesInfo[i].Name == currentEvent)
 				currentIndex = i;
@@ -98,7 +103,7 @@ public class InGameMenuScript: MonoBehaviour {
 			//the file is loaded elsewhere. All that script needs is the name of the new file.
 		}
 	}
-
+	
 	public void LoadPrevious(){
 		string currentEvent = PlayerPrefs.GetString("File To Load");
 		DirectoryInfo dir = new DirectoryInfo(Application.streamingAssetsPath);
