6882516c487eb6aeb8cf2650bbdcc1a62f4c53d9
OwenCrawford <owen@owens-air.dhcp.fnal.gov>
2015-07-27 12:57:37 -0500

    Online events menu work

--- Assets/Scripts/Interface/Event Menu/OnlineEventsMenu.cs
+++ Assets/Scripts/Interface/Event Menu/OnlineEventsMenu.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-
+using System.Collections.Generic;
 
 
 public class OnlineEventsMenu : MonoBehaviour {
@@ -13,52 +13,72 @@ public class OnlineEventsMenu : MonoBehaviour {
 	public GameObject gammaPanel;
 
     public GameObject buttonsGroup;
-	
     string url1 = "http://argo-microboone.fnal.gov/server/serve_event.cgi?entry=0&filename=%252Fpnfs%252Fuboone%252Fscratch%252Fuboonepro%252Fmcc6.0%252Fv04_06_01%252Freco1%252Fprod";
-    string[] url2Array = new Array("_bnblike_electron", "_bnblike_pi0", "_bnblike_proton", "_bnblike_muminus", "_bnb_like_gamma");
+    string[] url2Array = new string[]{"_bnblike_electron", "_bnblike_pi0", "_bnblike_proton", "_bnblike_muminus", "_bnb_like_gamma"};
     string url3 = "_uboone%252F";
-    string[] url4Array = new Array("1691317_", "1691318_", "1831337_", "1695054_", "1831485_");
-    int[][] url5Array = [[0,2,3,4,5], [0,1,2,3,4], [1,3,4,5,6], [0,1,3,4,5], [0,2,3,4,5]];
+    string[] url4Array = new string[]{"1691317_", "1691318_", "1831337_", "1695054_", "1831485_"};
+    int[][] url5Array = {new int[]{0,2,3,4,5}, new int[]{0,1,2,3,4}, new int[]{1,3,4,5,6}, new int[]{0,1,3,4,5}, new int[]{0,2,3,4,5}};
     string url6 = "%252Fprod_*&options=_NoPreSpill_NoPostSpill__NORAW__NOCAL_";
+    string[][] buttons;
 	
 	void Start(){
-		HidePanels();
-		ShowElectronEvents();
+        buttons = new string[url2Array.Length][];
+        for(int particle = 0; particle < url2Array.Length; particle++){
+            string[] partButtons = new string[url5Array[particle].Length];
+            for(int evn = 0; evn < url5Array[particle].Length; evn++){
+                partButtons[evn] =  url1 + url2Array[particle] + url3 + url4Array[particle] + url5Array[particle][evn] + url6 ;
+            }
+            buttons[particle] = partButtons;
+        }
+        Debug.Log(buttons.Length);
+
 	}
 
-	void HidePanels(){
-		electronPanel.SetActive(false);
-		pi0Panel.SetActive(false);
-		protonPanel.SetActive(false);
-		muminusPanel.SetActive(false);
-		gammaPanel.SetActive(false);
-	}
+//	void HidePanels(){
+//		electronPanel.SetActive(false);
+//		pi0Panel.SetActive(false);
+//		protonPanel.SetActive(false);
+//		muminusPanel.SetActive(false);
+//		gammaPanel.SetActive(false);
+//	}
+
+    void ClearButtons(){
+        for(int i=0; i<buttonsGroup.transform.childCount; i++)
+            Destroy(buttonsGroup.transform.GetChild(i));
+    }
 	
 	public void ShowElectronEvents(){
-		HidePanels();
-		electronPanel.SetActive(true);
+        ClearButtons();
+        foreach(string file in buttons[0])
+            AddButton("abutton", file);
 	}
 	
 	public void ShowPi0Events(){
-		HidePanels();
-		pi0Panel.SetActive(true);
+        ClearButtons();
+        foreach(string file in buttons[1])
+            AddButton("abutton", file);
 	}
 
 	public void ShowProtonEvents(){
-		HidePanels();
-		protonPanel.SetActive(true);
+        ClearButtons();
+        foreach(string file in buttons[2])
+            AddButton("abutton", file);
 	}
 	
 	public void ShowMuminusEvents(){
-		HidePanels();
-		muminusPanel.SetActive(true);
+        ClearButtons();
+        foreach(string file in buttons[3])
+            AddButton("abutton", file);
 	}
 	
 	public void ShowGammaEvents(){
-		HidePanels();
-		gammaPanel.SetActive(true);
+        ClearButtons();
+        foreach(string file in buttons[4])
+            AddButton("abutton", file);
 	}
 
-
+    void AddButton(string Text, string FileName){
+        Debug.Log ("adding a button");
+    }
 
 }
