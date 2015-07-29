e81ef3b20ac376ea30f66c990cece05045349bc5
OwenCrawford <owen@owens-air.dhcp.fnal.gov>
2015-07-29 08:59:09 -0500

    adjusted dot scaling

--- Assets/Scripts/Camera Scaling/ScaleRelativeToCamera.cs
+++ Assets/Scripts/Camera Scaling/ScaleRelativeToCamera.cs
@@ -8,16 +8,16 @@ using System.Collections;
 public class ScaleRelativeToCamera : MonoBehaviour 
 {
 //	public Camera cam;
-	public float objectScale = 1.0f; 
+    public float objectScale; // = 1.0f; 
 
 	//Make sure the object is never bigger or smaller than these factors
-	public float maxScale = 5.0f;
-	public float minScale = 1f;
+    public float maxScale; // = 5.0f;
+    public float minScale; //= 1f;
 
 	//These control when the max scale and min scale are reached
 	//e.g. max scale at distance of 12, min scale at distance of 3 opposed to raw scaling on distance.
-	public float minDist = 6.0f;
-	public float maxDist = 12.0f;
+    public float minDist; // = 6.0f;
+    public float maxDist; // = 12.0f;
 
 	private Vector3 initialScale; 
 	
