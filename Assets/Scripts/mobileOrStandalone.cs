50b44a6efc0b07777102028211160eac69d5dd04
OwenCrawford <owen@owens-air.dhcp.fnal.gov>
2015-08-03 12:38:23 -0500

    auto-disable some models in mobile
    and auto-reenable for standalone

---
+++ Assets/Scripts/mobileOrStandalone.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mobileOrStandalone : MonoBehaviour {
+
+	public GameObject fieldCage;
+	public GameObject feedthrough1;
+	public GameObject feedthrough2;
+
+	void Start () {
+#if MOBILE_INPUT
+		fieldCage.SetActive(false);
+		feedthrough1.SetActive(false);
+		feedthrough2.SetActive(false);
+#else
+		fieldCage.SetActive(true);
+		feedthrough1.SetActive(true);
+		feedthrough2.SetActive(true);
+#endif
+	}	
+}
