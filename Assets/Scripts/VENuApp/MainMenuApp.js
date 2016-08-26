293689a8502c2b885fcf9277159b826d0cac4598
Marco Del Tutto <marco.deltutto@physics.ox.ac.uk>
2016-08-26 18:18:08 +0200

    Adding scenes for the VENu app.

---
+++ Assets/Scripts/VENuApp/MainMenuApp.js
@@ -0,0 +1,25 @@
+﻿// MainMenuApp.js
+//
+// created by Marco Del Tutto, marco.deltutto@physics.ox.ac.uk
+
+
+#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function goToEventDisplay() {
+
+}
+
+function goToCardboardEventDisplay() {
+
+  Application.LoadLevel("DisplayCardboardApp");
+
+}
+
