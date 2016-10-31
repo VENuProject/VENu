506f97d568d44221046e164fb0aa03bf7005ad20
Marco Del Tutto <marco.deltutto@physics.ox.ac.uk>
2016-10-31 00:30:32 +0000

    Change in game: double click on tracks to move to next event.

--- Assets/Scripts/VENuApp/gameApp.cs
+++ Assets/Scripts/VENuApp/gameApp.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿// gameApp.css
+//
+// created by Marco Del Tutto, marco.deltutto@physics.ox.ac.uk
+
+using UnityEngine;
 using System.Collections;
 using System.IO;
 using UnityEngine.UI;
@@ -8,7 +12,7 @@ using UnityEngine.SceneManagement;
 
 public class gameApp : MonoBehaviour {
 
-	[Tooltip("Rate the -Continue your serach...- will flash. In seconds.")]
+	[Tooltip("Rate the -Continue your search...- will flash. In seconds.")]
 	public float flashingRate = 5.0F;
 
 	private GameObject canvas;
@@ -17,8 +21,15 @@ public class gameApp : MonoBehaviour {
 	private bool goToMainMenu;
 	private bool goToNextWithCosmics;
 
-	private float thisTime;
-	private float previousTime;
+	//private float thisTime;
+	//private float previousTime;
+
+	private int numberOfClicks = 0;
+	private int doubleClicked = 2;
+	private double previousClickTime = -1;
+	private double thisClickTime = -1;
+	[Tooltip("Multiple click on track sensitivity. (seconds between two clicks)")]
+	public double multipleClickSpeed = 1;
 
 
 	// Use this for initialization
@@ -26,7 +37,7 @@ public class gameApp : MonoBehaviour {
 
 		Screen.orientation = ScreenOrientation.LandscapeLeft;
 
-		previousTime = Time.time;
+		//previousTime = Time.time;
 
 	}
 	
@@ -57,48 +68,72 @@ public class gameApp : MonoBehaviour {
 	}*/
 
 	void OnMouseDown(){
-		goToNext = false;
-		goToMainMenu = false;
-		goToNextWithCosmics = false;
 
-		// Check what is the active prefab and decide to go back to main menu or not.
-		evtPrefab = GameObject.Find("EventsPrefab_simulation");
-		Debug.Log ("the name is " + evtPrefab.name);
-		foreach (Transform child in evtPrefab.transform) {
-			Debug.Log ("The name of the child is " + child.name);
-			if (child.gameObject.activeInHierarchy) {
-				// For the turorial
-				if (child.name == "prodgenie_bnb_nu_uboone_new_1.json_testGame") goToNext = true;
-				if (child.name == "prodgenie_bnb_nu_uboone_new_2.json_testGame") goToNextWithCosmics = true;
-				if (child.name == "prodgenie_bnb_nu_cosmic_uboone_game_final_10.json") goToNext = true;
-				if (child.name == "prodgenie_bnb_nu_cosmic_uboone_game_final_12.json") goToMainMenu = true;
+		numberOfClicks++;
 
-				// For the real game
-				if (child.name == "prodgenie_bnb_nu_cosmic_uboone_game_final_2.json") goToNext = true;
-				if (child.name == "prodgenie_bnb_nu_cosmic_uboone_game_final_6.json") goToNext = true;
-				if (child.name == "prodgenie_bnb_nu_cosmic_uboone_game_final_7.json") goToMainMenu = true;
-				if (child.name == "prodgenie_bnb_nu_cosmic_uboone_game_final_8.json") goToNext = true;
-				if (child.name == "prodgenie_bnb_nu_cosmic_uboone_game_final_9.json") goToNext = true;
+		thisClickTime = Time.time;
+		double deltaTime = thisClickTime - previousClickTime;
 
+		// If I clicked at least twice, and the last two clicks happend close in time to each other, 
+		//then it means I double clicked, then do whatever you have to do
+		if (numberOfClicks >= doubleClicked && deltaTime < multipleClickSpeed) {
+
+			numberOfClicks = 0;
+
+			goToNext = false;
+			goToMainMenu = false;
+			goToNextWithCosmics = false;
+
+			// Check what is the active prefab and decide to go back to main menu or not.
+			evtPrefab = GameObject.Find ("EventsPrefab_simulation");
+			Debug.Log ("the name is " + evtPrefab.name);
+			foreach (Transform child in evtPrefab.transform) {
+				Debug.Log ("The name of the child is " + child.name);
+				if (child.gameObject.activeInHierarchy) {
+					// For the turorial
+					if (child.name == "prodgenie_bnb_nu_uboone_new_1.json_testGame")
+						goToNext = true;
+					if (child.name == "prodgenie_bnb_nu_uboone_new_2.json_testGame")
+						goToNextWithCosmics = true;
+					if (child.name == "prodgenie_bnb_nu_cosmic_uboone_game_final_10.json")
+						goToNext = true;
+					if (child.name == "prodgenie_bnb_nu_cosmic_uboone_game_final_12.json")
+						goToMainMenu = true;
+
+					// For the real game
+					if (child.name == "prodgenie_bnb_nu_cosmic_uboone_game_final_2.json")
+						goToNext = true;
+					if (child.name == "prodgenie_bnb_nu_cosmic_uboone_game_final_6.json")
+						goToNext = true;
+					if (child.name == "prodgenie_bnb_nu_cosmic_uboone_game_final_7.json")
+						goToMainMenu = true;
+					if (child.name == "prodgenie_bnb_nu_cosmic_uboone_game_final_8.json")
+						goToNext = true;
+					if (child.name == "prodgenie_bnb_nu_cosmic_uboone_game_final_9.json")
+						goToNext = true;
+
+				}
 			}
-		}
 
 
-		// Activate congrats panel
-		canvas = GameObject.Find("GameCanvas");
-		Debug.Log ("the name is " + canvas.name);
-		foreach (Transform child in canvas.transform)
-		{
-			Debug.Log ("The name of the child is " + child.name);
-			if (child.name == "PanelContinueSearch")
-				child.gameObject.SetActive (false);
-			if (child.name == "PanelNext" && goToNext)
-				child.gameObject.SetActive (true);	
-			if (child.name == "PanelGoToMainMenu" && goToMainMenu)
-				child.gameObject.SetActive (true);
-			if (child.name == "PanelNextWithCosmics" && goToNextWithCosmics)
-				child.gameObject.SetActive (true);
+			// Activate congrats panel
+			canvas = GameObject.Find ("GameCanvas");
+			Debug.Log ("the name is " + canvas.name);
+			foreach (Transform child in canvas.transform) {
+				Debug.Log ("The name of the child is " + child.name);
+				if (child.name == "PanelContinueSearch")
+					child.gameObject.SetActive (false);
+				if (child.name == "PanelNext" && goToNext)
+					child.gameObject.SetActive (true);	
+				if (child.name == "PanelGoToMainMenu" && goToMainMenu)
+					child.gameObject.SetActive (true);
+				if (child.name == "PanelNextWithCosmics" && goToNextWithCosmics)
+					child.gameObject.SetActive (true);
+			}
+
 		}
+
+		previousClickTime = Time.time;
 
 
 	}
